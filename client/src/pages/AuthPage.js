24df5658fdb4d4e88ea2712b3153d69be44ce508
Andriy Shendryk <avshendrik@gmail.com>
2021-03-20 12:21:37 +0200

    authpage

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHttp } from '../hooks/http.hook';
 
 export const AuthPage = () => {
+    const { loading, request } = useHttp();
+
+    const [form, setForm ] = useState({
+        email: '',
+        password: ''
+    })
+
+    const changeHandler = event => {
+        setForm({ ...form, [event.target.name]: event.target.value })
+    }
+
+    const registerHandler = async () => {
+        try {
+            const data = await request('/api/auth/register', 'POST', {...form});
+            console.log('Data', data)
+        } catch (e) {
+            
+        }
+    }
+
+    
+
     return (
-        <div>
-            <h1>Auth Page</h1>   
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <h1>Make link shorter</h1>
+                <div className="card blue darken-1">
+                    <div className="card-content white-text">
+                        <span className="card-title">Autorization</span>
+                        <div>
+                            <div className="input-field">
+                                <input 
+                                placeholder="Type your email" 
+                                id="email" 
+                                type="text"
+                                name="email"
+                                className="yellow-input"
+                                onChange={changeHandler}
+                                />
+                                <label htmlFor="email">Email</label>
+                            </div>
+
+                            <div className="input-field">
+                                <input 
+                                placeholder="Type your password" 
+                                id="password" 
+                                type="password"
+                                name="password"
+                                className="yellow-input"
+                                onChange={changeHandler}
+                                />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-action">
+                        <button 
+                        className="btn yellow darken-4"
+                        style={{marginRight: 10}}
+                        disabled={loading}
+                        >
+                            Login
+                        </button>
+                        <button 
+                        className="btn grey lighten-1 black-text"
+                        onClick={registerHandler}
+                        disabled={loading}
+                        >
+                            Register
+                        </button>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
