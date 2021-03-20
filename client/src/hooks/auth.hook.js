8921f59ac00d9e29dffbf7d26860ef481dbc24e4
Andriy Shendryk <avshendrik@gmail.com>
2021-03-20 19:47:19 +0200

    navbar add

--- client/src/hooks/auth.hook.js
+++ client/src/hooks/auth.hook.js
@@ -1,12 +1,32 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
+
+const storageName = 'userData';
 
 export const useAuth = () => {
   const [token, setToken] = useState(null);
   const [userId, setUserId] = useState(null);
   
-  const login = useCallback(() => {});
+  const login = useCallback((id, jwtToken) => {
+    setToken(jwtToken);
+    setUserId(id);
+    localStorage.setItem(storageName, JSON.stringify({
+      userId: id, token: jwtToken
+    }));
+  }, []);
 
-  const logout = useCallback(() => {});
+  const logout = useCallback(() => {
+    setToken(null);
+    setUserId(null);
+    localStorage.removeItem(storageName);
+  }, []);
 
-  return { login, logout }
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem(storageName));
+
+    if(data && data.token) {
+      login(data.userId, data.token);
+    }
+  }, [login])
+
+  return { login, logout, token, userId }
 }
