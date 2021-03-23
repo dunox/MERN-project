0b9426016aa2ba4ca59074b2b6504d9b786d6760
Andriy Shendryk <avshendrik@gmail.com>
2021-03-23 10:27:39 +0200

    add UI and logic to create page

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext';
+import { useHttp } from '../hooks/http.hook';
 
 export const CreatePage = () => {
-
+    const auth = useContext(AuthContext);
+    const { request } = useHttp();
+    const history = useHistory();
     const [ link, setLink ] = useState('');
 
-    const pressHandler = event => {
+    useEffect(() => {
+        window.M.updateTextFields();
+    });
+
+
+    const pressHandler = async event => {
         if(event.key === 'Enter') {
             try {
-                
+                const data = await request('/api/link/generate', 'POST', {from: link}, {
+                    Authorization: `Bearer ${auth.token}`
+                  })
+                  history.push(`detail/${data.link._id}`)
             } catch (e) {}
         }
     }
@@ -25,7 +38,6 @@ export const CreatePage = () => {
                 />
                 <label htmlFor="link">Enter Link</label>
             </div>
-            <h1>Create Page</h1>   
         </div>
     )
 }
--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -2,13 +2,13 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 module.exports = (req, res, next) => {
-  if( req.metho === 'OPTIONS') {
+  if( req.method === 'OPTIONS') {
     return next();
   }
 
   try {
     
-    const token = req.headers.authorization.split('')[1] // "Bearer TOKEN"
+    const token = req.headers.authorization.split(' ')[1] // "Bearer TOKEN"
 
     if(!token) {
       return res.status(401).json({message: "Non Autorization"});
